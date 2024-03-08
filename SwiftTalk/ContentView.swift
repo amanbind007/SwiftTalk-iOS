4330dcf06edc41d91c57a65ed609a8ba273fce4f
Aman <amanbind007@gmail.com>
2024-03-08 22:02:30 +0530

    Added custom font and Changed navigation bar font style

--- SwiftTalk/ContentView.swift
+++ SwiftTalk/ContentView.swift
@@ -8,31 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewView = false
+    
+    init(){
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.black, .font: UIFont(name: "AbrilFatface-Regular", size: 25)!]
+        
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.black, .font: UIFont(name: "AbrilFatface-Regular", size: 40)!]
+        
+    }
+
     var body: some View {
         NavigationView(content: {
-            
             HomeView()
                 .navigationTitle("SwiftTalk")
                 .navigationBarTitleDisplayMode(.automatic)
                 .toolbar(content: {
                     ToolbarItem {
-                        Button(action: {}, label: {
+                        Button(action: {
+                            showNewView = true
+                        }, label: {
                             Image(systemName: "plus.circle")
                         })
                     }
                 })
 
         })
+        .sheet(isPresented: $showNewView, content: {
+            NewTextView()
+        })
     }
 }
 
 struct HomeView: View {
     @State private var search: String = ""
-    
+
     var body: some View {
         VStack {
-            
-            
             List {
                 ForEach(["aman", "bind", "kumar"], id: \.self) { data in
                     Text(data)
---
+++ SwiftTalk/NewTextView.swift
@@ -0,0 +1,53 @@
+//
+//  NewTextView.swift
+//  SwiftTalk
+//
+//  Created by Aman Bind on 08/03/24.
+//
+
+import SwiftUI
+
+struct NewTextView: View {
+    @State private var text = ""
+    
+    @Environment(\.colorScheme) private var theme
+    
+    init(){
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.black, .font: UIFont(name: "AbrilFatface-Regular", size: 25)!]
+    }
+    
+    var body: some View {
+        NavigationView(content: {
+            VStack {
+                Divider()
+                TextEditor(text: $text)
+                    .scrollContentBackground(.hidden)
+                    
+                    //.background()
+            }
+            .navigationTitle("SwiftTalk")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(content: {
+                ToolbarItem {
+                    Button(action:  {
+                        
+                    }, label: {
+                        Text("Save")
+                    })
+                }
+            })
+            .background(
+                Color.red
+            )
+            
+        })
+        .presentationCornerRadius(25)
+        
+        
+    }
+        
+}
+
+#Preview {
+    NewTextView()
+}
