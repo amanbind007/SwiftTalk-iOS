f994becad1b40d9a883522fd264aaaea9b775e2e
Aman <amanbind007@gmail.com>
2024-04-09 12:45:39 +0530

    Created the AddnewTextViewModel to hold the State variables

--- SwiftTalk/ViewModel/AddNewTextViewModel.swift
+++ SwiftTalk/ViewModel/AddNewTextViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftUI
+import UIKit
+
+@Observable
+class AddNewTextViewModel {
+    
+    var isPlaying = false
+    var isPopoverPresented = false
+    var degreesRotating = 0.0
+
+    let pasteboard = UIPasteboard.general
+    
+    var title = ""
+    var text = """
+    """
+
+    
+}
--- SwiftTalk/Views/AddNewTextView.swift
+++ SwiftTalk/Views/AddNewTextView.swift
@@ -9,38 +9,15 @@ import AVFoundation
 import SwiftUI
 
 struct AddNewTextView: View {
-    @State private var title = ""
-    @State private var isPlaying = false
-    @State private var isPopoverPresented = false
-    
-    let pasteboard = UIPasteboard.general
-    
-    @State private var text = """
-    
-    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-    Why do we use it?
-    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-
-    Where does it come from?
-    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-
-    Where can I get some?
-    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-    """
-
     @Environment(\.colorScheme) private var theme
     @Environment(\.dismiss) private var dismiss
     
-    @State var degreesRotating = 0.0
-    
+    @State var addNewTextVM = AddNewTextViewModel()
+
     var body: some View {
         VStack {
-            // TextFileds for title and Contents
             
+            // TextFileds for title and Contents
             VStack {
                 Rectangle()
                     .frame(height: 1)
@@ -50,7 +27,7 @@ struct AddNewTextView: View {
                 HStack {
                     Text("Title:")
                         .font(.custom(Constants.Fonts.NotoSerifR, size: 18))
-                    TextField("", text: $title)
+                    TextField("", text: $addNewTextVM.title)
                         .font(.custom(Constants.Fonts.NotoSerifR, size: 18))
                 }
                 .scrollContentBackground(.hidden)
@@ -62,30 +39,28 @@ struct AddNewTextView: View {
                     .foregroundStyle(
                         LinearGradient(colors: [.pink, .purple], startPoint: .leading, endPoint: .trailing)
                     )
-                TextEditor(text: $text)
+                TextEditor(text: $addNewTextVM.text)
                     .font(.custom(Constants.Fonts.NotoSerifR, size: 18))
             }
             
             // Bottom Control Panel
-            
             VStack {
                 HStack {
                     ZStack {
                         RippledCircle()
-                            
                             .fill(LinearGradient(colors: [.pink, .blue, .purple], startPoint: .topLeading, endPoint: .bottomTrailing))
                             .frame(width: 55, height: 55)
-                            .rotationEffect(.degrees(degreesRotating))
+                            .rotationEffect(.degrees(addNewTextVM.degreesRotating))
                             .onAppear(perform: {
                                 withAnimation(.linear(duration: 1)
                                     .speed(0.5).repeatForever(autoreverses: false))
                                 {
-                                    degreesRotating = 360.0
+                                    addNewTextVM.degreesRotating = 360.0
                                 }
                                 
                             })
                             .onTapGesture {
-                                isPopoverPresented.toggle()
+                                addNewTextVM.isPopoverPresented.toggle()
                             }
                         
                         Image("myPhoto2")
@@ -93,23 +68,26 @@ struct AddNewTextView: View {
                             .aspectRatio(contentMode: .fit)
                             .frame(width: 45, height: 45)
                             .clipShape(Circle())
+                            .onTapGesture {
+                                addNewTextVM.isPopoverPresented.toggle()
+                            }
                     }
-                    .popover(isPresented: $isPopoverPresented, content: {
+                    .popover(isPresented: $addNewTextVM.isPopoverPresented, content: {
                         VoiceSelectorView()
                             .presentationCompactAdaptation(.automatic)
                     })
                     
                     Spacer()
                     
-                    Image(systemName: isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                    Image(systemName: addNewTextVM.isPlaying ? "pause.circle.fill" : "play.circle.fill")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 50, height: 50)
-                        .foregroundColor(isPlaying ? .red : .blue)
+                        .foregroundColor(addNewTextVM.isPlaying ? .red : .blue)
                         .clipShape(Circle())
                         .onTapGesture {
                             withAnimation {
-                                isPlaying.toggle()
+                                addNewTextVM.isPlaying.toggle()
                             }
                         }
                     
@@ -134,7 +112,7 @@ struct AddNewTextView: View {
             ToolbarItemGroup {
                 HStack {
                     Button(role: .destructive) {
-                        text = ""
+                        addNewTextVM.text = ""
                     } label: {
                         Image(Constants.Icons.backspaceicon)
                             .resizable()
@@ -142,8 +120,8 @@ struct AddNewTextView: View {
                     }
                     
                     Button(role: .none) {
-                        if let pasteString = pasteboard.string {
-                            text = pasteString
+                        if let pasteString = addNewTextVM.pasteboard.string {
+                            addNewTextVM.text = pasteString
                         }
                         
                     } label: {
@@ -157,10 +135,14 @@ struct AddNewTextView: View {
                         
                         dismiss()
                     } label: {
+                        
                         Image(Constants.Icons.saveIcon)
                             .resizable()
                             .frame(width: 25, height: 25)
+                            .grayscale(addNewTextVM.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? 1 : 0)
+                            
                     }
+                    .disabled(addNewTextVM.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? true : false)
                 }
             }
             
