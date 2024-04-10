dcf838cda22c42f9334df77196298a373f754017
Aman <amanbind007@gmail.com>
2024-04-10 22:09:04 +0530

    VoiceSpeedPitchSelector View uses the new custom slider now + Custom Heading Bar

---
+++ SwiftTalk/Managers/SpeechManager.swift
@@ -0,0 +1,8 @@
+//
+//  SpeechManager.swift
+//  SwiftTalk
+//
+//  Created by Aman Bind on 10/04/24.
+//
+
+import Foundation
--- SwiftTalk/Views/VoiceSpeedPitchSelectorView.swift
+++ SwiftTalk/Views/VoiceSpeedPitchSelectorView.swift
@@ -11,58 +11,85 @@ struct VoiceSpeedPitchSelectorView: View {
     @State var voiceSpeedSliderValue = 1.0
     @State var voicePitchSliderValue = 1.0
     var body: some View {
-        VStack {
-            HStack {
-                switch voiceSpeedSliderValue {
-                case 0.5...1.0:
-                    Image(systemName: "gauge.with.dots.needle.0percent")
-                case 1.0...2.0:
-                    Image(systemName: "gauge.with.dots.needle.33percent")
-                case 2.0...3.0:
-                    Image(systemName: "gauge.with.dots.needle.50percent")
-                case 3.0...4.0:
-                    Image(systemName: "gauge.with.dots.needle.67percent")
-                case 4.0...5.0:
-                    Image(systemName: "gauge.with.dots.needle.100percent")
-                default:
-                    Image(systemName: "gauge.with.dots.needle.50percent")
+        ZStack {
+            Color.accent2
+                .ignoresSafeArea()
+            VStack {
+                Spacer(minLength: 30)
+                
+                HStack {
+                    switch voiceSpeedSliderValue {
+                    case 0.5...1.0:
+                        Image(systemName: "gauge.with.dots.needle.0percent")
+                    case 1.0...2.0:
+                        Image(systemName: "gauge.with.dots.needle.33percent")
+                    case 2.0...3.0:
+                        Image(systemName: "gauge.with.dots.needle.50percent")
+                    case 3.0...4.0:
+                        Image(systemName: "gauge.with.dots.needle.67percent")
+                    case 4.0...5.0:
+                        Image(systemName: "gauge.with.dots.needle.100percent")
+                    default:
+                        Image(systemName: "gauge.with.dots.needle.50percent")
+                    }
+                    
+                    Text("Utterence Speed \(trimValue(value: voiceSpeedSliderValue))x")
+                        .font(.custom(Constants.Fonts.NotoSerifR, size: 16))
                 }
-                    
-                Text("Utterence Speed \(trimValue(value: voiceSpeedSliderValue))x")
+                .symbolEffect(.bounce, value: voiceSpeedSliderValue)
+                .imageScale(.large)
+                
+                HStack {
+                    Text("0.5x")
+                        .font(.custom(Constants.Fonts.NotoSerifR, size: 16))
+                    LinearGradientSlider(value: $voiceSpeedSliderValue, range: 0.5...5, step: 0.5) { _ in
+                        // Update Default for speed
+                    }
+                    Text("5.0x")
+                        .font(.custom(Constants.Fonts.NotoSerifR, size: 16))
+                }
+                .padding()
+                .offset(y: -20)
+                
+                HStack {
+                    Image(systemName: "waveform.path")
+                    Text("Utterence Pitch \(trimValue(value: voicePitchSliderValue))x")
+                        .font(.custom(Constants.Fonts.NotoSerifR, size: 16))
+                }
+                .symbolEffect(.bounce, value: voicePitchSliderValue)
+                .imageScale(.large)
+                
+                HStack {
+                    Text("1.0x")
+                        .font(.custom(Constants.Fonts.NotoSerifR, size: 16))
+                    LinearGradientSlider(value: $voicePitchSliderValue, range: 1...5, step: 0.5) { _ in
+                        // Update Default for speed
+                    }
+                    Text("5.0x")
+                        .font(.custom(Constants.Fonts.NotoSerifR, size: 16))
+                }
+                .padding()
+                .offset(y: -20)
+                
             }
-            .symbolEffect(.bounce, value: voiceSpeedSliderValue)
-            .imageScale(.large)
-                    
-            Slider(value: $voiceSpeedSliderValue, in: 0.5...5, step: 0.5) {
-                Label("\(voiceSpeedSliderValue)", systemImage: "cloud")
-            } minimumValueLabel: {
-                Text("0.5x")
-            } maximumValueLabel: {
-                Text("5.0x")
-            } onEditingChanged: { _ in
-                // update Default for speed
-            }
-            .padding()
-            .offset(y: -20)
             
-            HStack {
-                Image(systemName: "waveform.path")
-                Text("Utterence Pitch \(trimValue(value: voicePitchSliderValue))x")
+            VStack {
+                VStack {
+                    HStack {
+                        Text("Select Voice")
+                            .font(.custom(Constants.Fonts.AbrilFatfaceR, size: 20))
+                            .offset(y: 10)
+                    }
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundStyle(
+                            LinearGradient(colors: [.pink, .purple], startPoint: .leading, endPoint: .trailing)
+                        )
+                }
+                .background(Material.ultraThin)
+                
+                Spacer()
             }
-            .symbolEffect(.bounce, value: voicePitchSliderValue)
-            .imageScale(.large)
-            
-            Slider(value: $voicePitchSliderValue, in: 1...5, step: 0.5) {
-                Label("\(voicePitchSliderValue)", systemImage: "cloud")
-            } minimumValueLabel: {
-                Text("1.0x")
-            } maximumValueLabel: {
-                Text("5.0x")
-            } onEditingChanged: { _ in
-                // update Default for speed
-            }
-            .padding()
-            .offset(y: -20)
         }
     }
     
@@ -72,5 +99,5 @@ struct VoiceSpeedPitchSelectorView: View {
 }
 
 #Preview {
-    VoiceSpeedSelector()
+    VoiceSpeedPitchSelectorView()
 }
