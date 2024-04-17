2095ccdb887d8140f8b39e91207ca466ed25d6e9
Aman <amanbind007@gmail.com>
2024-04-17 21:43:50 +0530

    created ViewModel for AddNewTextView to hold logic code

--- SwiftTalk/ViewModel/AddNewTextViewModel.swift
+++ SwiftTalk/ViewModel/AddNewTextViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftSoup
 import SwiftUI
 import UIKit
 
@@ -15,11 +16,50 @@ class AddNewTextViewModel {
     var isVoiceSelectorPresented = false
     var isVoiceSpeedSelectorPresented = false
     var degreesRotating = 0.0
-
+    
     let pasteboard = UIPasteboard.general
 
     var title = ""
     var text = """
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis interdum ex. Donec faucibus orci purus, at sagittis lorem placerat at. Morbi sodales porta massa in blandit. Cras eget varius lacus. Cras ac libero quis velit semper dapibus. Maecenas viverra mauris nec maximus egestas. In hac habitasse platea dictumst. Quisque hendrerit scelerisque lorem sit amet pellentesque. Mauris luctus ipsum in pharetra finibus. Sed rutrum finibus dui. Mauris ullamcorper ut ante vel congue. Nullam ac justo sagittis, tincidunt velit id, consequat velit. Vivamus purus turpis, condimentum eu blandit nec, ullamcorper et erat. Fusce velit eros, ornare at lectus quis, interdum varius urna.
     """
+    
+    var link: String = ""
+    var isParsingWebText: Bool = false
+
+    func getTextFromLink(){
+        isParsingWebText = true
+        
+        if let url = URL(string: link) {
+            URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    self.isParsingWebText = false
+                } else {
+                    if let data = data {
+                        let html = String(data: data, encoding: .utf8)!
+                        
+                        do {
+                            let doc: Document = try SwiftSoup.parse(html)
+                            guard let body = doc.body() else { return }
+                            let text = try body.text()
+                            
+                           
+                            
+                            self.text = text
+                            
+                            self.isParsingWebText = false
+                            
+                        } catch {
+                            print(error)
+                            self.isParsingWebText = false
+                        }
+                    }
+                }
+            }.resume()
+        }else{
+            isParsingWebText = false
+        }
+        
+    }
 }
