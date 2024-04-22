4e143e7453d126e7fee4422cd0b63da30a57068b
Aman <amanbind007@gmail.com>
2024-04-22 22:51:19 +0530

    added properties for different sheets

--- SwiftTalk/ViewModel/AddNewTextViewModel.swift
+++ SwiftTalk/ViewModel/AddNewTextViewModel.swift
@@ -6,9 +6,11 @@
 //
 
 import Foundation
+import PhotosUI
 import SwiftSoup
 import SwiftUI
 import UIKit
+import Vision
 
 @Observable
 class AddNewTextViewModel {
@@ -24,10 +26,15 @@ class AddNewTextViewModel {
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce iaculis interdum ex. Donec faucibus orci purus, at sagittis lorem placerat at. Morbi sodales porta massa in blandit. Cras eget varius lacus. Cras ac libero quis velit semper dapibus. Maecenas viverra mauris nec maximus egestas. In hac habitasse platea dictumst. Quisque hendrerit scelerisque lorem sit amet pellentesque. Mauris luctus ipsum in pharetra finibus. Sed rutrum finibus dui. Mauris ullamcorper ut ante vel congue. Nullam ac justo sagittis, tincidunt velit id, consequat velit. Vivamus purus turpis, condimentum eu blandit nec, ullamcorper et erat. Fusce velit eros, ornare at lectus quis, interdum varius urna.
     """
     
+    // Web Link Text Sheet View Properties
     var link: String = ""
     var isParsingWebText: Bool = false
-
-    func getTextFromLink(){
+    
+    // Image Text Sheet View Properties
+    var selectedImages: [UIImage] = []
+    var isProcessingImages: Bool = false
+    
+    func getTextFromLink() {
         isParsingWebText = true
         
         if let url = URL(string: link) {
@@ -44,8 +51,6 @@ class AddNewTextViewModel {
                             guard let body = doc.body() else { return }
                             let text = try body.text()
                             
-                           
-                            
                             self.text = text
                             
                             self.isParsingWebText = false
@@ -57,9 +62,8 @@ class AddNewTextViewModel {
                     }
                 }
             }.resume()
-        }else{
+        } else {
             isParsingWebText = false
         }
-        
     }
 }
