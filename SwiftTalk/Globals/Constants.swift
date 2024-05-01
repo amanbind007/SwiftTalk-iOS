a61fa961b9d89a8f9cd70a90ce22ed4577408c28
Aman <amanbind007@gmail.com>
2024-05-01 21:41:43 +0530

    removed removed constants

--- SwiftTalk/Globals/Constants.swift
+++ SwiftTalk/Globals/Constants.swift
@@ -11,16 +11,11 @@ class Constants {
     enum Fonts {
         static let NotoSerifSB = "NotoSerif-SemiBold"
         static let NotoSerifR = "NotoSerif-Regular"
-        static let AbrilFatfaceR = "AbrilFatface-Regular"
         static let ChangaOneR = "ChangaOne"
     }
 
     enum Icons {
-        static let backspaceicon = "backspace_icon"
         static let cameraIcon = "camera_icon"
-        static let cancelIcon = "cancel_icon"
-        static let clipboardIcon = "clipboard_icon"
-        static let saveIcon = "floppy_disk"
         static let imageFileIcon = "image_file_icon"
         static let linkIcon = "link_icon"
         static let pdfFileIcon = "pdf_file_icon"
@@ -28,6 +23,5 @@ class Constants {
         static let textFileInputIcon = "text_input_file_icon"
         static let webIcon = "web_icon"
         static let wordFileIcon = "word_file_icon"
-        static let pptFileIcon = "ppt_file_icon"
     }
 }
