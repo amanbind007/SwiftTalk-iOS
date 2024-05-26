f0ef00e6825652a4533df247d50a34d3ac757631
Aman <amanbind007@gmail.com>
2024-05-26 23:15:03 +0530

    created button and function to play the voice demo

--- SwiftTalk/Views/VoiceSelectorListItemView.swift
+++ SwiftTalk/Views/VoiceSelectorListItemView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct VoiceSelectorListItemView: View {
     var voice: Voice
@@ -13,14 +14,22 @@ struct VoiceSelectorListItemView: View {
     @AppStorage("selectedVoice") var selectedVoice = "Trinoids"
     @AppStorage("language") var language = "en-US"
     
-    @State var moveAround = false
+    @State var play = false
     
     var body: some View {
         HStack {
+            Image(systemName: selectedVoice == voice.voiceName ? "checkmark.circle.fill" : "circle.fill")
+                .foregroundStyle(selectedVoice == voice.voiceName ? Color.green : Color.secondary)
+                .onTapGesture {
+                    selectedVoice = voice.voiceName
+                    language = voice.languageCode
+                }
+                .imageScale(.large)
+            Divider()
             Image(voice.flagName)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
-                .frame(width: 25, height: 25)
+                .frame(width: 26, height: 26)
             
             Divider()
             
@@ -40,39 +49,41 @@ struct VoiceSelectorListItemView: View {
             
             Divider()
             
-            ZStack {
-                Circle()
-                    .phaseAnimator([false, true]) { _, moveAround in
-                        Circle()
-                            .stroke(style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round, miterLimit: 0, dash: [1, 3], dashPhase: moveAround ? -10 : 150))
-                            .frame(width: 26, height: 26)
-                            .foregroundStyle(.linearGradient(colors: [.red, .green], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    } animation: { _ in
-                        .linear.speed(0.05).repeatForever(autoreverses: false)
-                    }
-
-                Image(systemName: "play.circle.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 22, height: 22)
-                    .clipShape(Circle())
-            }
-            
             Text(voice.voiceName)
                 .font(.custom(Constants.Fonts.NotoSerifR, size: 14))
             
             Spacer()
             
-            Image(systemName: selectedVoice == voice.voiceName ? "checkmark.circle.fill" : "circle.fill")
-                .foregroundStyle(selectedVoice == voice.voiceName ? Color.green : Color.secondary)
-                .onTapGesture {
-                    selectedVoice = voice.voiceName
-                    language = voice.languageCode
+            Button(action: {
+                play.toggle()
+                if play {
+                    playDemoVoice(voice: voice)
                 }
+            }, label: {
+                ZStack {
+                    Image(systemName: play ? "stop.circle.fill" : "play.circle.fill")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 24, height: 24)
+                        .clipShape(Circle())
+                        .foregroundStyle(play ? .red : .blue)
+                }
+            })
         }
     }
+    
+    func playDemoVoice(voice: Voice){
+        let synthesizer = AVSpeechSynthesizer()
+        let utterrence = AVSpeechUtterance(string: voice.demoText)
+        utterrence.voice = AVSpeechSynthesisVoice(language: voice.languageCode)
+
+        synthesizer.speak(utterrence)
+        synthesizer.pauseSpeaking(at: .immediate)
+    }
+    
+    
 }
 
 #Preview {
-    VoiceSelectorListItemView(voice: Voice(languageCode: "ar-001", voiceName: "Majed", flagName: "saudi-arabia-flag-round-circle-icon", country: "Saudi Arabia", language: "AR"))
+    VoiceSelectorListItemView(voice: Voice(languageCode: "ar-001", voiceName: "Majed", flagName: "saudi-arabia-flag-round-circle-icon", country: "Saudi Arabia", language: "AR", demoText: "مرحبا بالعالم! هذا هو صوت م أجِد لتجربة ميزة تحويل النص إلى كلام"))
 }
