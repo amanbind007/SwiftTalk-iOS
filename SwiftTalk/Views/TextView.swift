902fa518911be0c14da2b82fde80e94c6d5a6df2
Aman <amanbind007@gmail.com>
2024-06-18 23:17:33 +0530

    gesture to detect tapped word in textView

--- SwiftTalk/Views/TextView.swift
+++ SwiftTalk/Views/TextView.swift
@@ -15,6 +15,8 @@ struct TextView: UIViewRepresentable {
     @Binding var text: String
     @Binding var highlightedRange: NSRange
 
+    var onCharacterTapped: ((Int) -> Void)? // Add a callback for character tapped
+
     func makeCoordinator() -> TextViewCoordinator {
         TextViewCoordinator(parent: self)
     }
@@ -28,6 +30,10 @@ struct TextView: UIViewRepresentable {
         textView.textAlignment = .left
         textView.autocorrectionType = .no
         textView.autocapitalizationType = .sentences
+        textView.isEditable = false
+
+        let tapGesture = UITapGestureRecognizer(target: context.coordinator, action: #selector(context.coordinator.handleTap(_:)))
+        textView.addGestureRecognizer(tapGesture)
 
         return textView
     }
@@ -35,14 +41,13 @@ struct TextView: UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
         uiView.font = UIFont.systemFont(ofSize: CGFloat(18), weight: .heavy)
         let attrStr = NSMutableAttributedString(string: text)
-        
+
         attrStr.addAttribute(NSAttributedString.Key.font, value: UIFont(name: Constants.Fonts.NotoSerifR, size: 18)!, range: NSRange(location: 0, length: attrStr.length))
 
-
         attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.black, range: NSRange(location: 0, length: attrStr.length))
-        
+
         attrStr.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor(.green), range: highlightedRange)
-        
+
         uiView.attributedText = attrStr
         uiView.scrollRangeToVisible(highlightedRange)
     }
@@ -54,12 +59,28 @@ class TextViewCoordinator: NSObject, UITextViewDelegate {
     init(parent: TextView) {
         self.parent = parent
     }
-    
+
     func textViewDidChange(_ textView: UITextView) {
         DispatchQueue.main.async {
             self.parent.text = textView.text
         }
     }
+
+    @objc func handleTap(_ gesture: UITapGestureRecognizer) {
+        let textView = gesture.view as! UITextView
+
+        let location = gesture.location(in: textView)
+        let position = CGPoint(x: location.x, y: location.y)
+        let tapPosition = textView.closestPosition(to: position)
+        let tappedRange = textView.tokenizer.rangeEnclosingPosition(tapPosition!, with: UITextGranularity.word, inDirection: UITextDirection(rawValue: 1))
+        var tappedWord: String?
+        if tappedRange != nil {
+            tappedWord = textView.text(in: tappedRange!)
+        }
+        print("tapped word: \(tappedWord)")
+    }
+    
+    
 }
 
 #Preview {
