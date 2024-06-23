fa9bba3aa671abcc4e9a04c1b0a15053953fe8a2
Aman <amanbind007@gmail.com>
2024-06-24 00:13:47 +0530

    Updated Text Editing View

--- SwiftTalk/Views/AddNewTextView.swift
+++ SwiftTalk/Views/AddNewTextView.swift
@@ -21,9 +21,11 @@ struct AddNewTextView: View {
     
     @AppStorage("SelectedVoice") var voice = "Trinoids"
     @AppStorage("selectedVoiceFlag") var selectedVoiceFlagIcon = "usa-flag-round-circle-icon"
-    
-    // @Environment(AddNewTextViewModel.self) var addNewTextVM
+
     @Bindable var addNewTextVM: AddNewTextViewModel
+    
+    @State var isEditing: Bool = false
+    @State var isFocused: Bool = false
     
     @State var showAlertTextNotSaved: Bool = false
     @State var showAlertNoText: Bool = false
@@ -46,125 +48,150 @@ struct AddNewTextView: View {
 
     var body: some View {
         VStack {
-            // TextFileds for title and Contents
-            VStack {
-                Divider()
+            VStack(alignment: .leading) {
                 HStack {
-                    Text("Title:")
-                        .font(.custom(Constants.Fonts.NotoSerifR, size: 18))
-                    TextField("", text: $addNewTextVM.title ?? "")
-                        .font(.custom(Constants.Fonts.NotoSerifR, size: 18))
+                    if isEditing {
+                        BannerButton(iconSystemName: "list.clipboard", color: .blue, text: "Paste")
+
+                            .onTapGesture {
+                                if let pasteString = addNewTextVM.pasteboard.string {
+                                    addNewTextVM.text = pasteString
+                                }
+                            }
+                          
+                        if let text = addNewTextVM.text {
+                            BannerButton(iconSystemName: "square.and.arrow.down", color: .green, text: "Save")
+                                
+                                .grayscale(text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? 1 : 0)
+                                .onTapGesture {
+                                    if verifyText(text: text) {
+                                        DataCoordinator.shared.saveObject(text: text, title: addNewTextVM.title, textSource: textSource)
+
+                                        dismiss()
+                                    }
+                                }
+                                .disabled(text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? true : false)
+                        }
+                        
+                        BannerButton(iconSystemName: "delete.backward", color: .red, text: "Delete")
+                            .onTapGesture {
+                                addNewTextVM.text = ""
+                            }
+                    }
                 }
-                .scrollContentBackground(.hidden)
-                
                 .padding(.horizontal, 5)
                 
                 Divider()
                 
-                Text("Add Text Below")
-                
-                
-                TextView(text: $addNewTextVM.text ?? "", highlightedRange: $speechManager.highlightedRange) { startIndex in
+                TextView(text: $addNewTextVM.text ?? "", highlightedRange: $speechManager.highlightedRange, editing: $isEditing, focused: $isFocused) { startIndex in
                     if let text = addNewTextVM.text {
                         speechManager.play(text: text, voice: voice, from: startIndex)
                     }
                 }
+                
+                Divider()
             }
             
-            // Bottom Control Panel
-            VStack {
-                HStack {
-                    Button(action: {
-                        addNewTextVM.isVoiceSelectorPresented.toggle()
+            if !isEditing {
+                // Bottom Control Panel
+                VStack {
+                    HStack {
+                        Button(action: {
+                            addNewTextVM.isVoiceSelectorPresented.toggle()
+                            
+                        }, label: {
+                            ZStack {
+                                RippledCircle()
+                                    .fill(
+                                        LinearGradient(
+                                            colors: [.pink, .blue, .purple],
+                                            startPoint: .topLeading,
+                                            endPoint: .bottomTrailing
+                                        )
+                                    )
+                                    .frame(width: 55, height: 55)
+                                
+                                Image(selectedVoiceFlagIcon)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 45, height: 45)
+                                    .clipShape(Circle())
+                            }
+                        })
                         
-                    }, label: {
-                        ZStack {
-                            RippledCircle()
+                        .sheet(isPresented: $addNewTextVM.isVoiceSelectorPresented, content: {
+                            VoiceSelectorView()
+                                .presentationCompactAdaptation(.automatic)
+                        })
+                        .sheet(isPresented: $addNewTextVM.isVoiceSpeedSelectorPresented, content: {
+                            VoiceSpeedPitchSelectorView()
+                                .presentationDetents([.height(280)])
+                        })
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // Go forward
+                        }, label: {
+                            Image(systemName: "goforward")
+                                .imageScale(.large)
+                        })
+                        
+                        Spacer()
+                        
+                        Image(systemName: addNewTextVM.isPlaying ? "pause.circle.fill" : "play.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50)
+                            .foregroundColor(addNewTextVM.isPlaying ? .red : .blue)
+                            .clipShape(Circle())
+                            .onTapGesture {
+                                withAnimation {
+                                    addNewTextVM.isPlaying.toggle()
+                                }
+                                
+                                if let text = addNewTextVM.text {
+                                    speechManager.play(text: text, voice: voice)
+                                }
+                            }
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            // Go forward
+                        }, label: {
+                            Image(systemName: "gobackward")
+                                .imageScale(.large)
+                        })
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            addNewTextVM.isVoiceSpeedSelectorPresented = true
+                            
+                        }, label: {
+                            FlowerCloud()
                                 .fill(
-                                    LinearGradient(
-                                        colors: [.pink, .blue, .purple],
-                                        startPoint: .topLeading,
+                                    .linearGradient(
+                                        colors: [.yellow, .green],
+                                        startPoint: .top,
                                         endPoint: .bottomTrailing
                                     )
                                 )
                                 .frame(width: 55, height: 55)
-
-                            Image(selectedVoiceFlagIcon)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 45, height: 45)
-                                .clipShape(Circle())
-                        }
-                    })
-                    
-                    .sheet(isPresented: $addNewTextVM.isVoiceSelectorPresented, content: {
-                        VoiceSelectorView()
-                            .presentationCompactAdaptation(.automatic)
-                    })
-                    .sheet(isPresented: $addNewTextVM.isVoiceSpeedSelectorPresented, content: {
-                        VoiceSpeedPitchSelectorView()
-                            .presentationDetents([.height(280)])
-                    })
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        // Go forward
-                    }, label: {
-                        Image(systemName: "goforward")
-                            .imageScale(.large)
-                    })
-                    
-                    Spacer()
-                    
-                    Image(systemName: addNewTextVM.isPlaying ? "pause.circle.fill" : "play.circle.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 50, height: 50)
-                        .foregroundColor(addNewTextVM.isPlaying ? .red : .blue)
-                        .clipShape(Circle())
-                        .onTapGesture {
-                            withAnimation {
-                                addNewTextVM.isPlaying.toggle()
-                            }
-                            
-                            if let text = addNewTextVM.text {
-                                speechManager.play(text: text, voice: voice)
-                            }
-                        }
-                    
-                    Spacer()
-                    
-                    Button(action: {}, label: {
-                        Image(systemName: "gobackward")
-                            .imageScale(.large)
-                    })
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                        addNewTextVM.isVoiceSpeedSelectorPresented = true
-                        
-                    }, label: {
-                        FlowerCloud()
-                            .fill(
-                                .linearGradient(
-                                    colors: [.yellow, .green],
-                                    startPoint: .top,
-                                    endPoint: .bottomTrailing
-                                )
-                            )
-                            .frame(width: 55, height: 55)
-                            .overlay {
-                                Text("2.5x")
-                                    .font(.title3)
-                                    .bold()
-                                    .foregroundStyle(Color.black)
-                            }
-                    })
+                                .overlay {
+                                    Text("2.5x")
+                                        .font(.title3)
+                                        .bold()
+                                        .foregroundStyle(Color.black)
+                                }
+                        })
+                    }
                 }
-                .padding(8)
             }
+        }
+        .onChange(of: speechManager.isSpeaking) {
+            print("Speech Manager Changed : \(speechManager.isSpeaking)")
         }
         .navigationBarTitleDisplayMode(.inline)
         .navigationTitle("SwiftTalk")
@@ -196,45 +223,18 @@ struct AddNewTextView: View {
                             Text("Back")
                         }
                     }
-                
-                    Button(role: .destructive) {
-                        addNewTextVM.text = ""
-                    } label: {
-                        Image(systemName: "delete.backward")
-                            .foregroundStyle(Color.red)
-                    }
                 }
             }
             
         })
         .toolbar(content: {
             ToolbarItemGroup(placement: .topBarTrailing) {
-                HStack {
-                    Button(role: .none) {
-                        if let pasteString = addNewTextVM.pasteboard.string {
-                            addNewTextVM.text = pasteString
+                CustomSwitch(iconSystemName: "pencil.line", colorOn: .green, colorOff: .red, text: "Edit", isOn: $isEditing)
+                    .onTapGesture {
+                        withAnimation(.linear) {
+                            isEditing = isEditing ? false : true
                         }
-
-                    } label: {
-                        Image(systemName: "list.clipboard")
                     }
-                    
-                    if let text = addNewTextVM.text {
-                        Button {
-                            if verifyText(text: text) {
-                                DataCoordinator.shared.saveObject(text: text, title: addNewTextVM.title, textSource: textSource)
-
-                                dismiss()
-                            }
-                            
-                        } label: {
-                            Image(systemName: "square.and.arrow.down")
-                            
-                                .grayscale(text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? 1 : 0)
-                        }
-                        .disabled(text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty ? true : false)
-                    }
-                }
             }
         })
         
