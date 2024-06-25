dddabe970527a4a2da3e695883b48380c79aff38
Aman <amanbind007@gmail.com>
2024-06-25 22:41:03 +0530

    TextData Title is not optional now

--- SwiftTalk/Model/TextData.swift
+++ SwiftTalk/Model/TextData.swift
@@ -11,13 +11,13 @@ import SwiftData
 @Model
 class TextData: Identifiable {
     @Attribute(.unique) var id: UUID
-    var textTitle: String?
+    var textTitle: String
     var text: String
     var textSource: AddNewTextOption
     var iconType: String
     var dateTime: Double
 
-    init(textTitle: String?, text: String, textSource: AddNewTextOption, iconType: String, dateTime: Double) {
+    init(textTitle: String, text: String, textSource: AddNewTextOption, iconType: String, dateTime: Double) {
         self.id = UUID()
         self.textTitle = textTitle
         self.text = text
