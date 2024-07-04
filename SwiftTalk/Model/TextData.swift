c80db79604016342dd448e93f374c67d88deacda
Aman <amanbind007@gmail.com>
2024-07-04 23:06:39 +0530

    updated the TextData Model

--- SwiftTalk/Model/TextData.swift
+++ SwiftTalk/Model/TextData.swift
@@ -11,18 +11,18 @@ import SwiftData
 @Model
 class TextData: Identifiable {
     @Attribute(.unique) var id: UUID
-    var textTitle: String
+    var textTitle: String?
     var text: String
     var textSource: AddNewTextOption
     var iconType: String
-    var dateTime: Double
+    var dateTime: Double?
 
-    init(textTitle: String, text: String, textSource: AddNewTextOption, iconType: String, dateTime: Double) {
+    init(textTitle: String?, text: String, textSource: AddNewTextOption, dateTime: Double?) {
         self.id = UUID()
         self.textTitle = textTitle
         self.text = text
         self.textSource = textSource
-        self.iconType = iconType
+        self.iconType = textSource.imageName
         self.dateTime = dateTime
     }
 }
