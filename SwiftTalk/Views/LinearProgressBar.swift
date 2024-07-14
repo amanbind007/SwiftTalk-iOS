1888422a483e868da4326e8e17b72363d6727a85
Aman <amanbind007@gmail.com>
2024-07-14 23:02:52 +0530

    created component: designed a linear progress bar

---
+++ SwiftTalk/Views/LinearProgressBar.swift
@@ -0,0 +1,60 @@
+//
+//  LinearProgressBar.swift
+//  SwiftTalk
+//
+//  Created by Aman Bind on 12/07/24.
+//
+
+import SwiftUI
+
+struct LinearProgressBar: View {
+    var value: Int
+    var total: Int
+    let color: Color
+
+    var progress: CGFloat {
+        return CGFloat(value) / CGFloat(total)
+    }
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 6)
+                    .foregroundColor(color.opacity(0.5))
+                    .overlay {
+                        HStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .foregroundColor(color)
+                                .padding(2)
+                                .frame(width: max(geometry.size.width * progress, 12), height: 12)
+                                .animation(.easeInOut(duration: 0.5), value: progress)
+
+                            Spacer(minLength: 0)
+                        }
+                    }
+            }
+            .frame(height: 12)
+        }
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: 30) {
+            LinearProgressBar(value: 1, total: 50, color: .red)
+
+            LinearProgressBar(value: 14, total: 50, color: .pink)
+
+            LinearProgressBar(value: 21, total: 50, color: .green)
+
+            LinearProgressBar(value: 28, total: 50, color: .mint)
+
+            LinearProgressBar(value: 35, total: 50, color: .blue)
+
+            LinearProgressBar(value: 42, total: 50, color: .purple)
+
+            LinearProgressBar(value: 50, total: 50, color: .secondary)
+        }
+        .padding()
+    }
+}
