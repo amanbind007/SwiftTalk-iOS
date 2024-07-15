a73784cf23bcc8f701cb28fa6cd4f09a313bcc71
Aman <amanbind007@gmail.com>
2024-07-15 21:49:49 +0530

    updated SwiftTalkApp dependency injection

--- SwiftTalk/SwiftTalkApp.swift
+++ SwiftTalk/SwiftTalkApp.swift
@@ -5,20 +5,15 @@
 //  Created by Aman Bind on 07/03/24.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct SwiftTalkApp: App {
-    @State var addNewTextVM = AddNewTextViewModel()
-
     var body: some Scene {
         WindowGroup {
             HomeView()
-                .environment(addNewTextVM)
-                
         }
         .modelContainer(for: TextData.self)
-        
     }
 }
