feb4f59cdbed13b70c1c1ed975ef7861f629ba9b
Aman <amanbind007@gmail.com>
2024-07-16 22:31:35 +0530

    Extension: extended color to be stored in userDefaults(appstorage)

---
+++ SwiftTalk/Extensions/Color+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Color+Extension.swift
+//  SwiftTalk
+//
+//  Created by Aman Bind on 15/07/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color: RawRepresentable {
+    public init?(rawValue: String) {
+        guard let data = Data(base64Encoded: rawValue) else{
+            self = .black
+            return
+        }
+        do{
+            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: UIColor.self, from: data) ?? .black
+            
+            self = Color(color)
+        }catch{
+            self = .black
+        }
+    }
+    public var rawValue: String {
+        do{
+            let data = try NSKeyedArchiver.archivedData(withRootObject: UIColor(self), requiringSecureCoding: false) as Data
+            return data.base64EncodedString()
+        }catch{
+            return ""
+        }
+    }
+}
