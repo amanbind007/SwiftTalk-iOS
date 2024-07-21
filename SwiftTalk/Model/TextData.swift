0fce4baa2ba7dd21b54b40ac3b83f84552af3757
Aman <amanbind007@gmail.com>
2024-07-21 23:11:07 +0530

    Added estimated reading time and showfetti shown/not-show tracker in TextData Model

--- SwiftTalk/Model/TextData.swift
+++ SwiftTalk/Model/TextData.swift
@@ -16,9 +16,43 @@ class TextData: Identifiable {
     var textSource: TextSource
     var dateTime: Double
     var progress: Int
+    var estimateReadTime: String {
+        let wordsPerMinute = 183
+        let wordCount = text.components(separatedBy: .whitespacesAndNewlines).filter { !$0.isEmpty }.count
+
+        let totalSeconds = Int((Double(wordCount) / Double(wordsPerMinute)) * 60)
+
+        let hours = totalSeconds / 3600
+        let minutes = (totalSeconds % 3600) / 60
+        let seconds = totalSeconds % 60
+
+        var result = ""
+
+        if hours > 0 {
+            result += "\(hours)hr "
+        }
+
+        if minutes > 0 || hours > 0 {
+            result += "\(minutes)min "
+        }
+
+        result += "\(seconds)sec"
+
+        return result.trimmingCharacters(in: .whitespaces)
+    }
+
     var timeSpend: Double
-    var isCompleted: Bool
+    var isCompleted: Bool {
+        if progress == text.count {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
     var completionDate: Double
+    var confettiShown: Bool
 
     init(textTitle: String?, text: String, textSource: TextSource) {
         self.id = UUID()
@@ -28,7 +62,7 @@ class TextData: Identifiable {
         self.dateTime = Date().timeIntervalSince1970
         self.progress = 0
         self.timeSpend = 0.0
-        self.isCompleted = false
         self.completionDate = 0.0
+        self.confettiShown = false
     }
 }
