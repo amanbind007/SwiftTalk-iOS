bf11dc50d45e6e8fe9aaf38429a5b4ad0ad0b866
Aman <amanbind007@gmail.com>
2024-07-25 22:27:36 +0530

    file rename + added color assets

--- SwiftTalk/Views/MainView.swift
+++ SwiftTalk/Views/MainView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct MainView: View {
     @State private var tabSelection = 1
+
+    init() {
+        UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Avenir-Heavy", size: 15)!], for: .normal)
+    }
+
     var body: some View {
         TabView(selection: $tabSelection) {
             HomeView()
                 .tabItem {
-                    Label("Home", systemImage: "house.fill")
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                        .font(.custom(Constants.Fonts.NotoSerifR, size: 10))
+                    // Label("Home", systemImage: "house.fill")
                 }
                 .tag(1)
 
