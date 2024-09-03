994f8f0668b4269aaa0e155df33bdb659ac8dfc6
Aman <amanbind007@gmail.com>
2024-09-03 23:24:40 +0530

    tweaks and changes for Reminder View

--- SwiftTalk/Views/ReminderView.swift
+++ SwiftTalk/Views/ReminderView.swift
@@ -29,94 +29,101 @@ struct ReminderView: View {
     }
     
     var body: some View {
-        VStack(spacing: 15) {
-            VStack(spacing: 1) {
-                HStack {
-                    Text("PREVIEW")
-                        .font(.callout)
-                        .foregroundStyle(.secondary)
-                    Spacer()
-                }
-                
-                HStack {
-                    Image(uiImage: UIImage.icon)
-                        .resizable()
-                        .frame(width: 45, height: 45)
+        NavigationView {
+            VStack(spacing: 15) {
+                VStack(spacing: 1) {
+                    HStack {
+                        Text("PREVIEW")
+                            .font(.footnote)
+                            .foregroundStyle(.secondary)
+                        Spacer()
+                    }
                     
-                    VStack {
-                        HStack {
-                            Text("Dummy Title")
-                                .font(.headline)
-                            Spacer()
-                            
-                            Text("now")
-                                .font(.callout)
-                                .foregroundStyle(.secondary)
-                        }
-                        HStack {
-                            Text("Dummy Text for Preview")
-                            
-                            Spacer(minLength: 0)
+                    HStack {
+                        Image(uiImage: UIImage.icon)
+                            .resizable()
+                            .frame(width: 45, height: 45)
+                        
+                        VStack {
+                            HStack {
+                                Text("Dummy Title")
+                                    .font(.headline)
+                                Spacer()
+                                
+                                Text("now")
+                                    .font(.callout)
+                                    .foregroundStyle(.secondary)
+                            }
+                            HStack {
+                                Text("Dummy Text for Preview")
+                                
+                                Spacer(minLength: 0)
+                            }
                         }
                     }
+                    .padding()
+                    .background(.thinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
                 }
-                .padding()
-                .background(.thinMaterial)
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-            }
-            
-            HStack(alignment: .center) {
-                DatePicker("Time", selection: $reminderTime, displayedComponents: .hourAndMinute)
-                    .pickerStyle(.wheel)
-            }
-
-            HStack {
-                if textData.selectedDays.isEmpty {
-                    Text("Not Scheduled")
-                } else if textData.selectedDays.count == 1 {
-                    Text(weekdays[textData.selectedDays.first!])
-                } else if textData.selectedDays.count == 7 {
-                    Text("Every day")
-                } else {
-                    Text(selectedWeekdaysString)
-                }
-
-                Spacer()
+                .padding(.bottom, 15)
                 
-                Toggle(isOn: $textData.hasReminder, label: {
-                    Text("")
-                })
-            }
-
-            HStack(spacing: 6) {
-                ForEach(0 ..< 7) { index in
-
-                    Button(action: {
-                        if textData.selectedDays.contains(index) {
-                            textData.selectedDays.remove(index)
-                        } else {
-                            textData.selectedDays.insert(index)
-                        }
-                    }) {
-                        HStack(spacing: 0) {
-                            Capsule()
-                                .foregroundStyle(Color.appTint)
-                                .opacity(textData.selectedDays.contains(index) ? 1 : 0.2)
-
-                                .frame(width: 47, height: 35)
-                                .overlay {
-                                    Text(weekdays[index].prefix(3))
-
-                                        .bold()
-                                        .foregroundStyle(textData.selectedDays.contains(index) ? Color.white : Color.secondary)
-                                        .dynamicTypeSize(.medium)
-                                }
+                HStack(alignment: .center) {
+                    DatePicker("Time", selection: $reminderTime, displayedComponents: .hourAndMinute)
+                        .pickerStyle(.wheel)
+                }
+                
+                HStack {
+                    if textData.selectedDays.isEmpty {
+                        Text("Not Scheduled")
+                    } else if textData.selectedDays.count == 1 {
+                        Text(weekdays[textData.selectedDays.first!])
+                    } else if textData.selectedDays.count == 7 {
+                        Text("Every day")
+                    } else {
+                        Text(selectedWeekdaysString)
+                    }
+                    
+                    Spacer()
+                    
+                    Toggle(isOn: $textData.hasReminder, label: {
+                        Text("")
+                    })
+                }
+                
+                HStack(spacing: 6) {
+                    ForEach(0 ..< 7) { index in
+                        
+                        Button(action: {
+                            if textData.selectedDays.contains(index) {
+                                textData.selectedDays.remove(index)
+                            } else {
+                                textData.selectedDays.insert(index)
+                            }
+                        }) {
+                            HStack(spacing: 0) {
+                                Capsule()
+                                    .foregroundStyle(Color.appTint)
+                                    .opacity(textData.selectedDays.contains(index) ? 1 : 0.2)
+                                
+                                    .frame(width: 47, height: 35)
+                                    .overlay {
+                                        Text(weekdays[index].prefix(3))
+                                        
+                                            .bold()
+                                            .foregroundStyle(textData.selectedDays.contains(index) ? Color.white : Color.secondary)
+                                            .dynamicTypeSize(.medium)
+                                    }
+                            }
                         }
                     }
                 }
+                Spacer()
             }
+            .padding()
+            .navigationTitle("Set Reminder")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
