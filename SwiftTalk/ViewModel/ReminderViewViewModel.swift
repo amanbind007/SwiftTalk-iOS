ff467a30919fa13e187dd684ad201f235e95edeb
Aman <amanbind007@gmail.com>
2024-09-07 22:59:21 +0530

    created Reminder ViewModel to separate view and logic

---
+++ SwiftTalk/ViewModel/ReminderViewViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  ReminderViewViewModel.swift
+//  SwiftTalk
+//
+//  Created by Aman Bind on 07/09/24.
+//
+
+import Foundation
+
+@Observable
+class ReminderViewViewModel {
+    var reminderTime: Date = .init()
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    
+    func selectedWeekDaysString(textData: TextData) -> String {
+        if textData.hasReminder {
+            if textData.selectedDays.isEmpty {
+                // calculate today or tommorrow
+                let calendar = Calendar.current
+                let now = Date()
+                print("Time Now : \(now.description)")
+                        
+                let dateComponents = calendar.dateComponents([.hour, .minute, .second], from: reminderTime)
+                let nowComponents = calendar.dateComponents([.hour, .minute, .second], from: now)
+                        
+                if let dateTime = calendar.date(from: dateComponents),
+                   let nowTime = calendar.date(from: nowComponents)
+                {
+                    if dateTime <= nowTime {
+                        return "today"
+                    } else {
+                        return "tomorrow"
+                    }
+                }
+                        
+                return "Error comparing times"
+            } else if textData.selectedDays.count == 1 {
+                return weekdays[textData.selectedDays.first!]
+            } else if textData.selectedDays.count == 7 {
+                return "Every day"
+            } else {
+                let sortedDays = textData.selectedDays.sorted()
+                var string = ""
+                for days in sortedDays {
+                    string += weekdays[days].prefix(3) + " "
+                }
+                string.removeLast()
+                
+                let weekdaysString = string.replacingOccurrences(of: " ", with: ", ")
+                
+                return weekdaysString
+            }
+        }
+        
+        return "Not Scheduled"
+    }
+}
