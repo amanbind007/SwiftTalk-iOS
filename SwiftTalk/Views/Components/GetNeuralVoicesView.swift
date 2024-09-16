f5feb07f6f1c6745c315a2c5fc3002120da61a27
Aman <amanbind007@gmail.com>
2024-09-16 23:30:30 +0530

    updated UI for help menu

--- SwiftTalk/Views/Components/GetNeuralVoicesView.swift
+++ SwiftTalk/Views/Components/GetNeuralVoicesView.swift
@@ -11,18 +11,48 @@ import YouTubePlayerKit
 struct GetNeuralVoicesView: View {
     let videoLink = "https://www.youtube.com/shorts/LyV7FNh3sDM"
     var body: some View {
-        YouTubePlayerView(
-            YouTubePlayer(
-                source: YouTubePlayer.Source.url(videoLink),
-                configuration: .init(useModestBranding:false)
-                
-            ),
-            placeholderOverlay: {
-                ProgressView()
+        List {
+            Section {
+                HStack {
+                    Image(uiImage: UIImage(named: "settingsIconIos")!)
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    Text("Go to settings")
+                }
             }
             
-        )
-        .padding()
+            Section {
+                HStack {
+                    Image(uiImage: UIImage(named: "accessibilityIconIos")!)
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    Text("Go to Accessibility")
+                }
+            }
+            
+            Section {
+                HStack {
+                    Image(uiImage: UIImage(named: "accessibilityIconIos")!)
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                    Text("Go to Live Speech")
+                }
+            }
+            
+        }
+            
+//            YouTubePlayerView(
+//                YouTubePlayer(
+//                    source: YouTubePlayer.Source.url(videoLink),
+//                    configuration: .init(useModestBranding:false)
+//
+//                ),
+//                placeholderOverlay: {
+//                    ProgressView()
+//                }
+//
+//            )
+//            .padding()
     }
 }
 
