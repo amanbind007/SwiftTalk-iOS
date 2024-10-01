65262e766284d0c81a62d23a3b70c746bc53a8ff
Aman <amanbind007@gmail.com>
2024-10-01 23:36:42 +0530

    created help view for user to download premium and enhanced voices

--- SwiftTalk/Views/Components/GetNeuralVoicesView.swift
+++ SwiftTalk/Views/Components/GetNeuralVoicesView.swift
@@ -14,45 +14,96 @@ struct GetNeuralVoicesView: View {
         List {
             Section {
                 HStack {
-                    Image(uiImage: UIImage(named: "settingsIconIos")!)
+                    Image(uiImage: UIImage(named: "SettingsIcon")!)
                         .resizable()
-                        .frame(width: 40, height: 40)
+                        .frame(width: 35, height: 35)
                     Text("Go to settings")
                 }
+            } footer: {
+                
             }
             
             Section {
                 HStack {
-                    Image(uiImage: UIImage(named: "accessibilityIconIos")!)
+                    Image(uiImage: UIImage(named: "AccessibilityIcon")!)
                         .resizable()
-                        .frame(width: 40, height: 40)
+                        .frame(width: 35, height: 35)
                     Text("Go to Accessibility")
                 }
             }
             
             Section {
                 HStack {
-                    Image(uiImage: UIImage(named: "accessibilityIconIos")!)
+                    Image(uiImage: UIImage(named: "LiveSpeechIcon")!)
                         .resizable()
-                        .frame(width: 40, height: 40)
+                        .frame(width: 35, height: 35)
                     Text("Go to Live Speech")
                 }
             }
             
-        }
+            Section{
+                Text("Select any voice in VOICES Section")
+                HStack{
+                    Image(uiImage: UIImage.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
             
-//            YouTubePlayerView(
-//                YouTubePlayer(
-//                    source: YouTubePlayer.Source.url(videoLink),
-//                    configuration: .init(useModestBranding:false)
-//
-//                ),
-//                placeholderOverlay: {
-//                    ProgressView()
-//                }
-//
-//            )
-//            .padding()
+            Section{
+                Text("Select voice of your choice from all the languages available")
+                HStack{
+                    Image(uiImage: UIImage.image2)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+            
+            Section{
+                Text("Download the premium and enhanced voices according to your preference")
+                HStack{
+                    Image(uiImage: UIImage.image3)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+            
+            Section{
+                Text("Once downloaded, it will appear in the voices lis of the app")
+                HStack{
+                    Image(uiImage: UIImage.image4)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+            }
+            
+            Section{
+                HStack{
+                    Text("Icon Meaning")
+                        .bold()
+                }
+                HStack{
+                    Image(uiImage: UIImage.eStar)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30)
+                    
+                    Text("Enhanced Voice (Good)")
+                }
+                HStack{
+                    Image(uiImage: UIImage.pStar)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30)
+                    
+                    Text("Premium Voice (Better)")
+                }
+            }
+        }
     }
 }
 
